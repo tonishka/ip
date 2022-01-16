9578312f050a46201895599ea1edbb61db7252c0
tonishka <tonishkasingh21@gmail.com>
2022-01-16 23:46:18 +0800

    Added echo and exit

--- src/main/java/Yoda.java
+++ src/main/java/Yoda.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Yoda {
     public static void main(String[] args) {
+
         System.out.println("Greetings Earthling.");
         pause();
         System.out.println("Yoda, I am.");
@@ -10,6 +13,15 @@ public class Yoda {
         pause();
         System.out.println("May the Force be with you, brave Jedi.");
 
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                echo(input);
+            }
+        }
     }
 
     private static void pause() {
@@ -18,5 +30,8 @@ public class Yoda {
         } catch(InterruptedException ie) {
             Thread.currentThread().interrupt();
         }
+    }
+    private static void echo(String msg) {
+        System.out.println(msg);
     }
 }
